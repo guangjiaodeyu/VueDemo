99c91c536055f1b96b7ab611e68e8e3c259d0b65
guangjiaodeyu <724709547@qq.com>
2017-07-29 10:10:56 +0800

    complete head and foot

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import LayOut from './components/layout'
+import VRouter from 'vue-router'
+import IndexPage from './pages/index'
 
-Vue.config.productionTip = false
+Vue.use(VRouter);
+let router = new VRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:IndexPage
+    }
+  ]
+});
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  template: '<LayOut/>',
+  components: { LayOut }
+});
